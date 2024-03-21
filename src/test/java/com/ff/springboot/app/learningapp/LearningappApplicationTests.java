167b408368359d05358f0ea6ef4ca63c7a86a3b9
Filipovic <filip.filipovic@zuehlke.com>
2024-03-21 14:19:27 +0100

    initial commit

---
+++ src/test/java/com/ff/springboot/app/learningapp/LearningappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.springboot.app.learningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
